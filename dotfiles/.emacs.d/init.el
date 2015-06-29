31e9ad5d337f7091eb860e46b1fd8a852a83d005
Gary Fenstamaker <fenstamaker@gmail.com>
2015-06-28 22:38:34 -0400

    New init

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -1,71 +1,242 @@
 (require 'package)
-
-(setq package-list '(undo-tree smartparens helm web-mode cider company flycheck rainbow-delimiters-mode))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")
+			 ("org" . "http://orgmode.org/elpa/")))
 
 (package-initialize)
 
+
+(setq package-list
+      '(use-package
+	saveplace
+	undo-tree
+	smartparens
+	helm
+	web-mode
+	cider
+	company
+	flycheck
+	rainbow-delimiters
+	))
+
 (unless package-archive-contents
-    (package-refresh-contents))
-    
+  (package-refresh-contents))
+
 (dolist (package package-list)
-    (unless (package-install-p package)
-        (package-install package)))
+  (unless (package-installed-p package)
+    (package-install package)))
 
-; === Funnies ===
+(package-initialize)
 
-(defun backup-file-name (fpath)
-    "Return a new file path of a given file path.
-     If the new path does not exist, create it."
-    (let* ( (backupRootDir "~/.emacs.d/backups/")
-            (filePath (replace-regexp-in-string "[A-Za-z]:" "" fpath))
-            (backupFilePath (replace-regexp-in-string "//" "/" (concat backupRootDir filePath "~"))) )
-        (make-directory (file-name-directory backupFilePath) (file-name-directory backupFilePath))
-        backupFilePath))
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
 
-(defun beginning-of-line-or-indentation ()
-    "Move cursor to beginning to line or the indentation."
-    (interactive)
-    (if (bolp)
-        (back-to-indentation)
-        (beginning-of-line)))
+(add-to-list 'load-path (concat user-emacs-directory "packages/"))
+
+;; Hide the startup splash screen
+(setq inhibit-startup-message t)
+
+;; Removes scratch message
+(setq initial-scratch-message nil)
+(setq initial-major-mode 'text-mode)
+
+;; Turn off annoying system bell
+(setq ring-bell-function 'ignore)
+
+;(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+;(load custom-file)
+
+;; Write backups to the backup directory in emacs.d
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name
+		 (concat user-emacs-directory "backups")))))
+
+(use-package saveplace
+	     :config
+	     (setq-default save-place t)
+	     (setq save-place-file (expand-file-name "places" user-emacs-directory)))
+
+;; Fix empty pasteboard error
+(setq save-interprogram-paste-before-kill nil)
 
 
+;; Full path in frame title
+(when window-system
+  (setq frame-title-format '(buffer-file-name "%f" ("%b"))))
 
-; === Packages ===
+;; Auto-refresh buffers when edits occur outside emacs
+(global-auto-revert-mode 1)
 
-(require 'undo-tree)
-(require 'smartparens-config)
-(require 'helm-config)
-(require 'web-mode)
-(require 'company)
-(require 'flycheck)
-(require 'cider)
+;; Also auto refresh dired, but be quiet
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
 
-;; __Package modes__
-(global-company-mode t)
-(global-undo-tree-mode t)
-(global-flycheck-mode t)
-(helm-mode t)
-(cider-mode t)
-(rainbow-delimiters-mode t)
+;; Show keystrokes in progress
+(setq echo-keystrokes 0.1)
 
-;; __Internal modes__
-(global-auto-revert-mode t)
-(global-linum-mode 1)
-(global-visual-line-mode 1)
+;; Move files to trash when deleting
+(setq delete-by-moving-to-trash t)
 
-(column-number-mode 1)
-(transient-mark-mode 1) 
+;; Transparently open compress files
+(auto-compression-mode t)
+
+;; Replace 'yes-or-no' with 'y-or-n'
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Use UTF-8 by default
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+(show-paren-mode 1)
+
+;; Remove text in selection when inserting text
 (delete-selection-mode 1)
-(subword-mode 1)
 
-;; __Hooks__
-(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+;; Always display line and column numbers
+(setq line-number-mode t)
+(setq column-number-mode t)
+(setq linum-format " %3d ")
+
+(setq transient-mark-mode t)
+
+;; Lines should be 80 characters wide, not 72 ???
+(setq fill-column 80)
+
+;; Smooth scroll (one line at a time)
+(setq mouse-wheel-scroll-amount '(1 ((shift) .1)))
+(setq mouse-wheel-progressive-speed nil)
+(setq mouse-wheel-follow-mouse 't)
+(setq scroll-step 15)
+
+;; Nicer scrolling with mouse wheel/trackpad.
+;; From Graphene
+(unless (and (boundp 'mac-mouse-wheel-smooth-scroll) mac-mouse-wheel-smooth-scroll)
+  (global-set-key [wheel-down] (lambda () (interactive) (scroll-up-command 1)))
+  (global-set-key [wheel-up] (lambda () (interactive) (scroll-down-command 1)))
+  (global-set-key [double-wheel-down] (lambda () (interactive) (scroll-up-command 2)))
+  (global-set-key [double-wheel-up] (lambda () (interactive) (scroll-down-command 2)))
+  (global-set-key [triple-wheel-down] (lambda () (interactive) (scroll-up-command 4)))
+  (global-set-key [triple-wheel-up] (lambda () (interactive) (scroll-down-command 4))))
+
+;; Scroll one line when hitting bottom of window
+(setq scroll-conservatively 10000)
+
+;; Change cursor
+(setq-default cursor-type 'box)
+(blink-cursor-mode -1)
+
+;; Do not insert tabs
+(set-default 'indent-tabs-mode nil)
+
+;; Navigate camelcase words
+(global-subword-mode 1)
+
+;; Turn off word wrap
+(setq-default truncate-lines t)
+
+;; Remove double space at end of sentence
+(set-default 'sentence-end-double-space nil)
+
+(use-package uniquify
+	   :config
+	   (setq uniquify-buffer-name-style 'forward))
 
 
-; === Aliases ===
+;; Org mode
 
+(use-package ob-core)
+(use-package ox-md)
+(use-package ox-latex)
+
+(use-package undo-tree
+	     :config
+	     (global-undo-tree-mode t))
+
+(use-package company
+	     :config
+	     (global-company-mode t)
+	     (setq company-idle-delay 0.125)
+	     (setq company-minimum-prefix-length 1))
+
+(use-package flycheck
+	     :config
+	     (global-flycheck-mode t))
+
+(use-package helm-config
+	     :config
+	     (helm-mode t)
+	     ;; __Fuzzers__
+	     
+	     (setq helm-M-x-fuzzy-match        t
+		   helm-buffers-fuzzy-matching t
+		   helm-recentf-fuzzy-match    t
+		   helm-semantic-fuzzy-match   t
+		   helm-imenu-fuzzy-match      t)
+	     
+	     ;; __Keybindings__
+	     
+	     (setq helm-split-window-in-side-p t)
+	     
+	     (global-unset-key (kbd "C-x c")) ; Remove the default helm key
+	     
+	     (global-set-key (kbd "s-h") 'helm-command-prefix)
+	     
+	     (global-set-key (kbd "M-x") 'helm-M-x)
+	     (global-set-key (kbd "s-;") 'helm-M-x)
+	     
+	     (global-set-key (kbd "M-y") 'helm-show-kill-ring)
+	     (global-set-key (kbd "s-y") 'helm-show-kill-ring)
+	     
+	     (global-set-key (kbd "C-x b") 'helm-mini)
+	     (global-set-key (kbd "s-b")   'helm-mini)
+	     
+	     (global-set-key (kbd "C-x C-f") 'helm-find-files)
+	     (global-set-key (kbd "s-f")     'helm-find-files)
+	     
+	     (global-set-key (kbd "C-s") 'helm-occur)
+	     (global-set-key (kbd "s-i") 'helm-semantic-or-imenu)
+	     
+	     (global-set-key (kbd "s-p") 'helm-projectile))
+
+(use-package cider
+	     :config
+	     (cider-mode t)
+	     (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode))
+
+(use-package rainbow-delimiters
+	     :config
+	     (rainbow-delimiters-mode t))
+
+(use-package web-mode
+	     :config
+	     (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+	     (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+	     (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+	     (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+	     (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+	     (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+	     (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+	     (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+	     
+	     (add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode)))
+
+(use-package smartparens
+	     :config
+	     (smartparens-global-mode t)
+	     (show-smartparens-global-mode t)
+	     (setq show-paren-style 'expression))
+
+(global-auto-revert-mode t)
+(global-linum-mode t)
+(global-visual-line-mode t)
+
+;; Aliases
 (defalias 'ff        'helm-find-files)
 (defalias 'files     'helm-find-files)
 (defalias 'imenu     'helm-semantic-or-imenu)
@@ -81,77 +252,6 @@
 (defalias 'repl      'cider)
 (defalias 'kill      'kill-buffer)
 
-; === Editor ===
-
-;; __GUI__
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(blink-cursor-mode -1)
-
-
-;; __Settings___
-
-; Change the backup files to ~/.emacs.d/backups/...
-(setq make-backup-file-name-function 'backup-file-name)
-
-(setq auto-save-default   nil)     ; Turn off auto-saves
-(setq line-move-visual    nil)     ; Move cursor down by visual line
-(setq indent-tabs-mode    nil)     ; Indents -> spaces
-
-(setq ring-bell-function  'ignore) ; Turn off sounds
-(setq inhibit-startup-message t)   ; Turn off startup message
-(setq linum-format " %4d ")        ; Line number format
-
-;;;; Scrolling
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) 
-(setq mouse-wheel-progressive-speed nil) 
-(setq mouse-wheel-follow-mouse 't) 
-(setq scroll-step 15)
-
-;; Nicer scrolling with mouse wheel/trackpad.
-;; From Graphene
-(unless (and (boundp 'mac-mouse-wheel-smooth-scroll) mac-mouse-wheel-smooth-scroll)
-  (global-set-key [wheel-down] (lambda () (interactive) (scroll-up-command 1)))
-  (global-set-key [wheel-up] (lambda () (interactive) (scroll-down-command 1)))
-  (global-set-key [double-wheel-down] (lambda () (interactive) (scroll-up-command 2)))
-  (global-set-key [double-wheel-up] (lambda () (interactive) (scroll-down-command 2)))
-  (global-set-key [triple-wheel-down] (lambda () (interactive) (scroll-up-command 4)))
-  (global-set-key [triple-wheel-up] (lambda () (interactive) (scroll-down-command 4))))
-
-;;;; Fonts
-(add-to-list 'default-frame-alist '(font . "Source Code Pro-15"))
-
-;;;; Company (Auto Complete)
-(setq company-idle-delay 0.125
-      company-minimum-prefix-length 1)
-
-
-; === Modes ===
-
-; web-mode
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-
-(add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
-
-
-; === Paren ===
-
-(show-paren-mode 1)
-(smartparens-global-mode t)
-(show-smartparens-global-mode t)
-
-; Highlights entire paren expression
-(setq show-parent-style 'expression)
-
-
-; === Text ===
 
 
 ; === Shorty ===
@@ -173,38 +273,19 @@
 (global-set-key (kbd "C-s-<up>")    'up-list)
 (global-set-key (kbd "C-s-<down>")  'down-list)
 
-; === Helm ====
-
-;; __Fuzzers__
-
-(setq helm-M-x-fuzzy-match        t
-      helm-buffers-fuzzy-matching t
-      helm-recentf-fuzzy-match    t
-      helm-semantic-fuzzy-match   t
-      helm-imenu-fuzzy-match      t)
-
-;; __Keybindings__
-
-(setq helm-split-window-in-side-p t)
-
-(global-unset-key (kbd "C-x c")) ; Remove the default helm key
-
-(global-set-key (kbd "s-h") 'helm-command-prefix)
-
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "s-;") 'helm-M-x)
-
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "s-y") 'helm-show-kill-ring)
-
-(global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "s-b")   'helm-mini)
-
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "s-f")     'helm-find-files)
-
-(global-set-key (kbd "C-s") 'helm-occur)
-(global-set-key (kbd "s-i") 'helm-semantic-or-imenu)
-
-(global-set-key (kbd "s-p") 'helm-projectile)
-
+;;;; Fonts
+(add-to-list 'default-frame-alist '(font . "Source Code Pro-15"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (uniquify web-mode use-package undo-tree smartparens rainbow-delimiters helm flycheck company cider))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ dotfiles/.emacs.d/old.el
@@ -0,0 +1,224 @@
+(require 'package)
+
+(setq package-list '(undo-tree smartparens helm web-mode cider company flycheck rainbow-delimiters))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(package-initialize)
+
+(unless package-archive-contents
+    (package-refresh-contents))
+    
+(dolist (package package-list)
+    (unless (package-installed-p package)
+        (package-install package)))
+
+; === Funnies ===
+
+(defun backup-file-name (fpath)
+    "Return a new file path of a given file path.
+     If the new path does not exist, create it."
+    (let* ( (backupRootDir "~/.emacs.d/backups/")
+            (filePath (replace-regexp-in-string "[A-Za-z]:" "" fpath))
+            (backupFilePath (replace-regexp-in-string "//" "/" (concat backupRootDir filePath "~"))) )
+        (make-directory (file-name-directory backupFilePath) (file-name-directory backupFilePath))
+        backupFilePath))
+
+(defun beginning-of-line-or-indentation ()
+    "Move cursor to beginning to line or the indentation."
+    (interactive)
+    (if (bolp)
+        (back-to-indentation)
+        (beginning-of-line)))
+
+
+
+; === Packages ===
+
+(require 'undo-tree)
+(require 'smartparens-config)
+(require 'helm-config)
+(require 'web-mode)
+(require 'company)
+(require 'flycheck)
+(require 'cider)
+
+;; __Package modes__
+(global-company-mode t)
+(global-undo-tree-mode t)
+(global-flycheck-mode t)
+(helm-mode t)
+(cider-mode t)
+(rainbow-delimiters-mode t)
+
+;; __Internal modes__
+(global-auto-revert-mode t)
+(global-linum-mode 1)
+(global-visual-line-mode 1)
+
+(column-number-mode 1)
+(transient-mark-mode 1) 
+(delete-selection-mode 1)
+(subword-mode 1)
+
+;; __Hooks__
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+
+
+; === Aliases ===
+
+(defalias 'ff        'helm-find-files)
+(defalias 'files     'helm-find-files)
+(defalias 'imenu     'helm-semantic-or-imenu)
+(defalias 'paste     'helm-show-kill-ring)
+(defalias 'resume    'helm-resume)
+(defalias 'search    'helm-occur)
+(defalias 'clipboard 'helm-show-kill-ring)
+(defalias 'comment   'comment-or-uncomment-region)
+(defalias 'start     'beginning-of-line)
+(defalias 'begin     'back-to-indentation)
+(defalias 'errors    'flycheck-list-errors)
+(defalias 'open-repl 'cider-jack-in)
+(defalias 'repl      'cider)
+(defalias 'kill      'kill-buffer)
+
+; === Editor ===
+
+;; __GUI__
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(blink-cursor-mode -1)
+
+
+;; __Settings___
+
+; Change the backup files to ~/.emacs.d/backups/...
+(setq make-backup-file-name-function 'backup-file-name)
+
+(setq auto-save-default   nil)     ; Turn off auto-saves
+(setq line-move-visual    nil)     ; Move cursor down by visual line
+(setq indent-tabs-mode    nil)     ; Indents -> spaces
+
+(setq ring-bell-function  'ignore) ; Turn off sounds
+(setq inhibit-startup-message t)   ; Turn off startup message
+(setq linum-format " %4d ")        ; Line number format
+
+;;;; Scrolling
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) 
+(setq mouse-wheel-progressive-speed nil) 
+(setq mouse-wheel-follow-mouse 't) 
+(setq scroll-step 15)
+
+;; Nicer scrolling with mouse wheel/trackpad.
+;; From Graphene
+(unless (and (boundp 'mac-mouse-wheel-smooth-scroll) mac-mouse-wheel-smooth-scroll)
+  (global-set-key [wheel-down] (lambda () (interactive) (scroll-up-command 1)))
+  (global-set-key [wheel-up] (lambda () (interactive) (scroll-down-command 1)))
+  (global-set-key [double-wheel-down] (lambda () (interactive) (scroll-up-command 2)))
+  (global-set-key [double-wheel-up] (lambda () (interactive) (scroll-down-command 2)))
+  (global-set-key [triple-wheel-down] (lambda () (interactive) (scroll-up-command 4)))
+  (global-set-key [triple-wheel-up] (lambda () (interactive) (scroll-down-command 4))))
+
+;;;; Fonts
+(add-to-list 'default-frame-alist '(font . "Source Code Pro-15"))
+
+;;;; Company (Auto Complete)
+(setq company-idle-delay 0.125
+      company-minimum-prefix-length 1)
+
+
+; === Modes ===
+
+; web-mode
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+(add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
+
+
+; === Paren ===
+
+(show-paren-mode 1)
+(smartparens-global-mode t)
+(show-smartparens-global-mode t)
+
+; Highlights entire paren expression
+(setq show-parent-style 'expression)
+
+
+; === Text ===
+
+
+; === Shorty ===
+
+(global-set-key (kbd "s-z")       'undo)
+(global-set-key (kbd "s-S-z")     'undo-tree-redo)
+(global-set-key (kbd "C-a")       'beginning-of-line-or-indentation)
+(global-set-key (kbd "s-/")       'comment-or-uncomment-region)
+(global-set-key (kbd "s-w")       'kill-buffer)
+
+;; __Navigation__
+
+(global-set-key (kbd "s-<up>")      'backward-paragraph)
+(global-set-key (kbd "s-<down>")    'forward-paragraph)
+(global-set-key (kbd "s-<left>")    'beginning-of-line-or-indentation)
+(global-set-key (kbd "s-<right>")   'end-of-line)
+(global-set-key (kbd "C-s-<right>") 'forward-sexp)
+(global-set-key (kbd "C-s-<left>")  'backward-sexp)
+(global-set-key (kbd "C-s-<up>")    'up-list)
+(global-set-key (kbd "C-s-<down>")  'down-list)
+
+; === Helm ====
+
+;; __Fuzzers__
+
+(setq helm-M-x-fuzzy-match        t
+      helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match    t
+      helm-semantic-fuzzy-match   t
+      helm-imenu-fuzzy-match      t)
+
+;; __Keybindings__
+
+(setq helm-split-window-in-side-p t)
+
+(global-unset-key (kbd "C-x c")) ; Remove the default helm key
+
+(global-set-key (kbd "s-h") 'helm-command-prefix)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "s-;") 'helm-M-x)
+
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "s-y") 'helm-show-kill-ring)
+
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "s-b")   'helm-mini)
+
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "s-f")     'helm-find-files)
+
+(global-set-key (kbd "C-s") 'helm-occur)
+(global-set-key (kbd "s-i") 'helm-semantic-or-imenu)
+
+(global-set-key (kbd "s-p") 'helm-projectile)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (rainbow-delimiters web-mode undo-tree smartparens helm flycheck company cider))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
